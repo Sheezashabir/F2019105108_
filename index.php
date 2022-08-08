b6cbf475585bf949cf5748e79c8f4e41071ad7cf
Sheezashabir <107406099+Sheezashabir@users.noreply.github.com>
2022-08-08 18:26:00 +0500

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if(isset($_SESSION['id'])){
-    header('Location:profile.php');
-}
-echo "<a href='register.php'>Create New Account</a></br>";
-echo "<a href='login.php'>Login to Your Account</a></br>";
